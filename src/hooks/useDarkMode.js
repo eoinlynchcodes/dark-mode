d9597a4244c0ba4e90f476d647d710aea5116fd9
eoinlynchcodes <eoin95lynch@gmail.com>
2020-02-12 16:02:52 +0000

    For submission.

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = () => {
+
+// Access dark.mode class.
+// Change style to darkmode on change of button.
+
+const accessor = document.getElementsByClassName("root");
+
+accessor.onClick(('', ) => {
+    
+})
+
+
+}
